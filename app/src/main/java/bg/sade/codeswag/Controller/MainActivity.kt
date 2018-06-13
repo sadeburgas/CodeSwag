89ecd1ae5050d7c52b61523fdc67f32f68fd2c11
Dilyan Ergeledzhiev <dergeledji@gmail.com>
2018-06-13 15:46:43 +0200

    ls 02 Simple ListView

--- app/src/main/java/bg/sade/codeswag/Controller/MainActivity.kt
+++ app/src/main/java/bg/sade/codeswag/Controller/MainActivity.kt
@@ -2,12 +2,23 @@ package bg.sade.codeswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import bg.sade.codeswag.Model.Category
 import bg.sade.codeswag.R
+import bg.sade.codeswag.Serviices.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter: ArrayAdapter<Category>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = ArrayAdapter(this,
+                android.R.layout.simple_list_item_1,
+                DataService.categories)
+        categoryListView.adapter = adapter
     }
 }
--- app/src/main/java/bg/sade/codeswag/Model/Category.kt
+++ app/src/main/java/bg/sade/codeswag/Model/Category.kt
@@ -1,3 +1,7 @@
 package bg.sade.codeswag.Model
 
-class Category(val tiitle: String, val image: String)
+class Category(val title: String, val image: String){
+    override fun toString(): String {
+        return title
+    }
+}
