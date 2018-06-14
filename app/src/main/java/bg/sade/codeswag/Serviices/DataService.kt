e8c3369b2dad7eb736d73d529359d59d1af97ff0
Dilyan Ergeledzhiev <dergeledji@gmail.com>
2018-06-14 22:00:13 +0200

    ls-08 recycle view adapter for products

--- app/src/main/java/bg/sade/codeswag/Serviices/DataService.kt
+++ app/src/main/java/bg/sade/codeswag/Serviices/DataService.kt
@@ -21,6 +21,15 @@ object DataService {
     val hats = listOf(
             Product("Devslopes Graphic Beanie", "$18", "hat1"),
             Product("Devslopes Hat Black", "$20", "hat2"),
+            Product("Devslopes Hat Snapback", "$22","hat4"),
+            Product("Devslopes Graphic Beanie", "$18", "hat1"),
+            Product("Devslopes Hat Black", "$20", "hat2"),
+            Product("Devslopes Hat Snapback", "$22","hat4"),
+            Product("Devslopes Graphic Beanie", "$18", "hat1"),
+            Product("Devslopes Hat Black", "$20", "hat2"),
+            Product("Devslopes Hat Snapback", "$22","hat4"),
+            Product("Devslopes Graphic Beanie", "$18", "hat1"),
+            Product("Devslopes Hat Black", "$20", "hat2"),
             Product("Devslopes Hat Snapback", "$22","hat4")
 
     )
@@ -28,9 +37,27 @@ object DataService {
             Product("Devslopes Hoodie Gray", "$28", "hoodie1"),
             Product("Devslopes Hoodie Red","$32","hoodie2"),
             Product("Devslopes Gray Hoodie","$28","hoodie3"),
+            Product("Devslopes Black Hoodie","$32","hoodie4"),
+            Product("Devslopes Hoodie Gray", "$28", "hoodie1"),
+            Product("Devslopes Hoodie Red","$32","hoodie2"),
+            Product("Devslopes Gray Hoodie","$28","hoodie3"),
+            Product("Devslopes Black Hoodie","$32","hoodie4"),
+            Product("Devslopes Hoodie Gray", "$28", "hoodie1"),
+            Product("Devslopes Hoodie Red","$32","hoodie2"),
+            Product("Devslopes Gray Hoodie","$28","hoodie3"),
             Product("Devslopes Black Hoodie","$32","hoodie4")
     )
     val shirts = listOf(
+            Product("Devslopes Shirt black","$18","shirt1"),
+            Product("Devslopes Badge Light Gray","$20","shirt2"),
+            Product("Devslopes Logo Shirt Red","$22","shirt3"),
+            Product("Devslopes Hustle","$22","shirt4"),
+            Product("Klickflip Studio","$32","shirt5"),
+            Product("Devslopes Shirt black","$18","shirt1"),
+            Product("Devslopes Badge Light Gray","$20","shirt2"),
+            Product("Devslopes Logo Shirt Red","$22","shirt3"),
+            Product("Devslopes Hustle","$22","shirt4"),
+            Product("Klickflip Studio","$32","shirt5"),
             Product("Devslopes Shirt black","$18","shirt1"),
             Product("Devslopes Badge Light Gray","$20","shirt2"),
             Product("Devslopes Logo Shirt Red","$22","shirt3"),
@@ -48,6 +75,6 @@ object DataService {
             "HOODIES"-> hoodies
             else -> digitalGood
         }
-        
+
     }
 }
